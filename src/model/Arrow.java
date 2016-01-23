9166b17e031a3afacba07ae822e885076bad5ff0
jeremy-deutsch <jeremydeutsch@email.arizona.edu>
2016-01-23 12:19:29 -0700

    Arrow Seems Pretty Set

--- src/model/Arrow.java
+++ src/model/Arrow.java
@@ -4,8 +4,8 @@ import java.awt.Point;
 
 
 public class Arrow {
-	private Node base, destination; //Connecting one idea to the next.
 	
+	private Node base, destination; //Connecting one idea to the next.
 	private Point start, end; //Where does it start, where does it end???
 	
 	public Arrow(Node base, Node destination) {
