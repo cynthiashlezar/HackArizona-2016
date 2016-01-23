4fc0a04b0beaacdc9a08b5396aaea9a980d7d10a
jeremy-deutsch <jeremydeutsch@email.arizona.edu>
2016-01-22 23:57:05 -0700

    Some Arrow Changes

--- src/Arrow.java
+++ src/Arrow.java
@@ -1,4 +1,15 @@
+import java.awt.Point;
 
 public class Arrow {
-	private Node base, destination; //Where does it start, where does it end???
+	private Node base, destination; //Connecting one idea to the next.
+	
+	private Point start, end; //Where does it start, where does it end???
+	
+	public Arrow(Node base, Node destination) {
+		this.base = base;
+		this.destination = destination;
+		
+	}
+	
+	
 }
