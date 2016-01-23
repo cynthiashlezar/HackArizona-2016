75148c1eb4f8725533bb109a1729a338af7d5e3b
cynthiashlezar <cynthiashlezar@live.com>
2016-01-22 23:55:29 -0700

    basic node gets/sets

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,38 @@
-
+import java.util.ArrayList;
 public abstract class Node {
-
+	
+	private String title;
+	private String text;
+	private ArrayList<Node> nodeRefs;
+	
+	public Node (String title, String text) {
+		this.title = title;
+		this.text = text;
+	}
+	
+	public String getTitle () {
+		return title;
+	}
+	
+	public String getText () {
+		return text;
+	}
+	
+	public ArrayList<Node> getNodeRefs () {
+		return nodeRefs;
+	}
+	
+	public void setTitle (String title) {
+		this.title = title;
+	}
+	
+	public void setText (String text) {
+		this.text = text;
+	}
+	
+	
+	
+	
+	
 }
+
