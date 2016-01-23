73f97313684097806a92fcd2fe6b63e16f53027c
Miranda Motsinger <mirandamotsinger@email.arizona.edu>
2016-01-23 00:07:32 -0700

    draftboard started

--- src/model/Draftboard.java
+++ src/model/Draftboard.java
@@ -1,5 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Draftboard {
+	
+	ArrayList<Node> nodes;
+	ArrayList<Arrow> arrows;
+	
+	public Draftboard() {
+		nodes = new ArrayList<Node>();
+		arrows = new ArrayList<Arrow>();
+	}
 
 }
--- src/model/Node.java
+++ src/model/Node.java
@@ -1,4 +1,7 @@
 package model;
+
 public abstract class Node {
+	
+	
 
 }
