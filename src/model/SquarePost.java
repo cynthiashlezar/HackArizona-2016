33497b6e7ebf2ad67cad0d9b6fbd6b312e37b1fa
cynthiashlezar <cynthiashlezar@live.com>
2016-01-23 00:33:43 -0700

    square class

---
+++ src/model/SquarePost.java
@@ -0,0 +1,32 @@
+package model;
+import java.util.ArrayList;
+import java.awt.Point;
+
+public class SquarePost extends Node {
+	
+	private String title;
+	private String text;
+	private String shape;
+	private Point location;
+	private ArrayList<Node> nodeRefs;
+	
+	public SquarePost (String title, String text, Point location) {
+		super(title, text, location);
+		shape = "circle.jpeg";
+	}
+
+	@Override
+	Point topRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Point botLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+
+
