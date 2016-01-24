343a3248cc13cf5fc8aae682260eb76e792da09e
cynthiashlezar <cynthiashlezar@live.com>
2016-01-23 18:20:56 -0700

    login/logout functionality

--- src/view/Chatbox.java
+++ src/view/Chatbox.java
@@ -17,61 +17,107 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import model.ChatMessage;
+import view.Chatbox.LoginListener;
 
 public class Chatbox extends JPanel {
 	
 	/*
-	 * Chatbox models the whole chat pane, including the actual field where messages appear,
-	 * the field where the user can enter a message, and a button to send the message.
-	 * 
-	 * Message log should be a JList drawing from an ArrayList<Message>. See EmailSearcher
-	 * Entry field should be a JTextField, see EmailSearcher
-	 * Button should be a JButton button = new JButton("Text")
-	 * Remember to put this.add(object) after each of these so it actually shows up on the panel.
-	 * Formatting is a goddamn pain in the ass, see DraftboardGUI for some formatting stuff and Google
-	 * for the rest.
-	 * 
-	 * SUPER BONUS: ACTION LISTENERS
-	 * The button needs an ActionListener that takes the contents of the JTextField, creates
-	 * a new Message, and adds it to the list of Messages (sends the message, basically).
-	 * Use button.addActionlistener(new SendListener()) and then write the send message
-	 * code in actionPerformed(ActionEvent e).
-	 */
-	//private ArrayList<ChatMessage> messageList;
+		 * Chatbox models the whole chat pane, including the actual field where messages appear,
+		 * the field where the user can enter a message, and a button to send the message.
+		 * 
+		 * Message log should be a JList drawing from an ArrayList<Message>. See EmailSearcher
+		 * Entry field should be a JTextField, see EmailSearcher
+		 * Button should be a JButton button = new JButton("Text")
+		 * Remember to put this.add(object) after each of these so it actually shows up on the panel.
+		 * Formatting is a goddamn pain in the ass, see DraftboardGUI for some formatting stuff and Google
+		 * for the rest.
+		 * 
+		 * SUPER BONUS: ACTION LISTENERS
+		 * The button needs an ActionListener that takes the contents of the JTextField, creates
+		 * a new Message, and adds it to the list of Messages (sends the message, basically).
+		 * Use button.addActionlistener(new SendListener()) and then write the send message
+		 * code in actionPerformed(ActionEvent e).
+		 */
+	private ArrayList<ChatMessage> messageList;
+
+	private JTextField usernameTextField;
+	private String username;
+	private JButton login;
+	private JLabel usernameLabel;
+	private JTextField userMessage;
+	private JButton enter;
+	private JButton logout;
+	private JTextArea aChat;
+	private JScrollPane theChat;
 	
-	
+
 	public Chatbox() {
-		JLabel usernameLabel = new JLabel("Username: ");
+		usernameLabel = new JLabel("Username: ");
 		this.add(usernameLabel);
-		JTextField usernameTextField = new JTextField();
+		usernameTextField = new JTextField();
 		usernameTextField.setColumns(5);
 		this.add(usernameTextField);
-		JButton login = new JButton("Login");
-		JButton logout = new JButton("Logout");
+		login = new JButton("Login");
+		logout = new JButton("Logout");
+		login.addActionListener(new LoginListener());
 		this.add(login);
 		this.add(logout);
-		JTextField userMessage = new JTextField();
-		userMessage.setColumns(6);
-		this.add(userMessage);
-		JButton enter = new JButton("Enter");
-		this.add(enter);
-		JList<String> messageLog;
-		JTextArea aChat = new JTextArea(20, 32);
+		logout.setEnabled(false);
+		logout.addActionListener(new LogoutListener());
+		aChat = new JTextArea(20, 32);
 		aChat.setEditable(false);
-		JScrollPane theChat = new JScrollPane(aChat);
+		theChat = new JScrollPane(aChat);
 		this.add(theChat);
 		//messageLog.add("hello!");
 		//this.add(messageLog);
+		userMessage = new JTextField();
+		userMessage.setColumns(6);
+		this.add(userMessage);
+		enter = new JButton("Enter");
+		this.add(enter);
+		enter.setEnabled(false);
 	}
 	
 	public class SendListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			ChatMessage message = new ChatMessage(userMessage.getText(), username);
+			messageList.add(message);
 			
 		}
 		
 	}
+	
+	public class LogoutListener implements ActionListener {
+		
+		public void actionPerformed(ActionEvent e) {
+			enter.setEnabled(false);
+			logout.setEnabled(false);
+			usernameLabel.setVisible(true);
+			usernameTextField.setVisible(true);
+			usernameLabel.setText("Username: ");
+			login.setVisible(true);
+			
+			
+		}
+	}
+	
+	public class LoginListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			enter.setEnabled(true);
+			username = usernameTextField.getText();
+			login.setVisible(false);
+			logout.setEnabled(true);
+			usernameTextField.setText("");
+			usernameTextField.setVisible(false);
+			usernameLabel.setText("Username: " + username);
+			
+		}
+
+	}
+
 
 }
